7ae82748c4bc75d78dcc34158563306b83b4f249
AmarS2001 <63029750+AmarS2001@users.noreply.github.com>
2022-01-04 15:21:01 +0530

    update
    update done to correct the code

--- Sliding_window.py
+++ Sliding_window.py
@@ -1,20 +1,18 @@
-lis= [0,0,1,1,0,1,1,1,0,1,1,1]
 def func(lis):
 	i=0
-	j=0
 	max_len=0
 	count=0
-	while( j < len(lis)):
-		if count !=2:
-			max_len=max(max_len,j-i+1)
-			j=j+1
-			if lis[j]==0:
-				count=count+1
-		else:
-			while lis[i] :
+	for j in range(0,len(lis)):
+		if lis[j]==0:
+			count=count+1
+		if count==2:
+			while lis[i]:
 				i=i+1
 			i=i+1
-			count = 1
+			count=1
+			
+		max_len=max(max_len, j-i+1)
 	return max_len	
-
 print(func(lis))
+
+## returns maximum length of the sequence of ones.
